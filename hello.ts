7202be8a8c180eec295db8138593daa587fd6fe2
Charles Wang <charleswang007@gmail.com>
2022-01-31 20:54:14 +0800

    vue change color

--- hello.ts
+++
@@ -1,9 +0,0 @@
-﻿function calc(price:number, tax:number):number {
-	return Math.floor(price * (1.0 + tax));
-}
-
-function doClick():void {
-	let text1:HTMLInputElement = document.querySelector('#text1');
-	let msg:Element = document.querySelector('#msg');
-	msg.innerHTML = calc(Number(text1.value), 0.05) + '元';
-}
---
+++ main.js
@@ -0,0 +1,17 @@
+﻿function initial(){
+	
+	new Vue({
+		el: '#msg',
+		data: {
+			message: 'this is Vue.js sample!',
+			text1: ''
+		} ,
+		methods: {
+			doAction: function(){
+				var str = this.text1;
+				this.message = 'you typed: <span style="color:white;background:red">'
+					+ str + '</span>.';
+			}
+		}
+	})
+}
